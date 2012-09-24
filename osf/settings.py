0636db2b7b115cb0eb6d50489916b88d1b0dcad7
Brenda J. Butler <bjb@sourcerer.ca>
2012-09-24 00:41:21 -0400

    setting for AUTH_PROFILE_MODULE

--- osf/settings.py
+++ osf/settings.py
@@ -134,6 +134,8 @@ INSTALLED_APPS = (
     'publicpages'
 )
 
+AUTH_PROFILE_MODULE = 'publicpages.UserProfile'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
